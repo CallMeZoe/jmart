ac94fd708877023211ef70649adcfb323b9e2586
CallMeZoe <ahmad.zufar91@ui.ac.id>
2021-09-20 21:08:17 +0700

    PT Modul 2 - Ahmad Zufar A

---
+++ ahmadZufarJsmartMH/Coupon.java
@@ -0,0 +1,58 @@
+package ahmadZufarJsmartMH;
+
+
+/**
+ * Write a description of class Coupon here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Coupon
+{
+    public enum Type{
+        DISCOUNT, REBATE
+    }
+    
+    public final String name;
+    public final int code;
+    public final double cut;
+    public final Type type;
+    public final double minimum;
+    private boolean used;
+    
+    public Coupon(String name, int code, Type type, double cut, double minimum){
+        this.name = name;
+        this.code = code;
+        this.type = type;
+        this.cut = cut;
+        this.minimum = minimum;
+        this.used = false;
+    }
+    
+    public boolean isUsed(){
+        return used;
+    }
+    
+    public boolean canApply(PriceTag priceTag){
+        if(priceTag.getAdjustedPrice() >= minimum && used == false){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    
+    public double apply(PriceTag priceTag){
+        used = true;
+        double value = priceTag.getAdjustedPrice();
+        if (type == Type.DISCOUNT){
+            return (100 - cut) / 100*value;
+        }
+        else if (type == Type.REBATE){
+             return value - priceTag.price;
+        }
+        else return 0.0;
+    }
+
+
+}
--- ahmadZufarJsmartMH/Jmart.java
+++ ahmadZufarJsmartMH/Jmart.java
@@ -11,13 +11,32 @@ public class Jmart
 {
     public static void main(String args[])
     {
-        System.out.println("Modul 2_Ahmad Zufar A_1906300662");
+        System.out.println("Modul 2 PT_Ahmad Zufar A_1906300662");
     }
 
-    public static Product create(){
+    /*public static Product create(){
         PriceTag priceTag = new PriceTag(80000);
         Product product = new Product("BUMI",500,false,priceTag,
         ProductCategory.BOOK);
         return product;
+    }*/
+    
+    public static Product createProduct()
+    {
+        PriceTag priceTag = new PriceTag(80000);
+        Product product = new Product("BUMI",500,false,priceTag,
+        ProductCategory.BOOK);
+        return product;
+    }
+
+    public static Coupon createCoupun()
+    {
+       Coupon coupon = new Coupon("BackToSchool",1, Coupon.Type.DISCOUNT, 10, 1000);
+       return coupon;
+    }
+
+    public static ShipmentDuration createShipmentDuration (String args[])
+    {
+        return new ShipmentDuration(ShipmentDuration.INSTANT, ShipmentDuration.REGULER);
     }
 }
